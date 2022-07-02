b43621b68bb7bbb7d9c3f21462454b9e2c03dfcc
icleveston <iurycl@gmail.com>
2022-07-02 18:03:45 -0300

    Added some functionalities

--- scripts/generate_stats.py
+++ scripts/generate_stats.py
@@ -7,38 +7,166 @@ from libpysal.cg import voronoi_frames
 
 
 def degree_analysis(graph):
+
+    print(f"Nodes: {len(graph.nodes)}")
+    print(f"Links: {len(graph.edges)}")
+
     in_degree_sequence = sorted((d for n, d in graph.in_degree()), reverse=True)
     out_degree_sequence = sorted((d for n, d in graph.out_degree()), reverse=True)
 
+    k_in, t_in = np.unique(in_degree_sequence, return_counts=True)
+    k_out, t_out = np.unique(out_degree_sequence, return_counts=True)
+    N_in, N_out = np.sum(t_in), np.sum(t_out)
+    p_k_in, p_k_out = t_in/N_in, t_out/N_out
+
+    print(f"Average Incoming Degree: {np.sum(k_in*p_k_in)}")
+    print(f"Average Outgoing Degree: {np.sum(k_out*p_k_out)}")
+
     fig = plt.figure("Degree Analysis", figsize=(8, 8))
 
-    axgrid = fig.add_gridspec(4, 4)
+    axgrid = fig.add_gridspec(2, 2)
 
-    ax1 = fig.add_subplot(axgrid[:2, :2])
-    ax1.plot(in_degree_sequence, "b-", marker="o")
-    ax1.set_title("In-degree Rank Plot")
-    ax1.set_ylabel("Degree")
-    ax1.set_xlabel("Rank")
+    ax1 = fig.add_subplot(axgrid[0, 0])
+    ax1.bar(k_in, p_k_in)
+    ax1.set_xlabel("k_in")
+    ax1.set_ylabel("degree distribution")
+    plt.grid()
+    plt.xlim([-0.5, max(in_degree_sequence)])
+    plt.ylim([0, 1])
+    plt.xticks(np.arange(min(in_degree_sequence), max(in_degree_sequence) + 1, 1.0))
+    plt.yticks(np.arange(0, 1+0.05, 0.1))
 
-    ax2 = fig.add_subplot(axgrid[:2, 2:])
-    ax2.bar(*np.unique(in_degree_sequence, return_counts=True))
-    ax2.set_title("In-degree Histogram")
-    ax2.set_xlabel("Degree")
-    ax2.set_ylabel("# of Nodes")
+    ax2 = fig.add_subplot(axgrid[0, 1])
+    ax2.scatter(k_in, p_k_in, s=100)
+    ax2.set_xlabel("k_in")
+    ax2.set_ylabel("degree distribution")
+    ax2.set_yscale('log')
 
-    ax3 = fig.add_subplot(axgrid[2:, :2])
-    ax3.plot(out_degree_sequence, "b-", marker="o")
-    ax3.set_title("Out-degree Rank Plot")
-    ax3.set_ylabel("Degree")
-    ax3.set_xlabel("Rank")
+    plt.xlim([-0.5, max(in_degree_sequence)+0.5])
+    plt.ylim([0.0001, 1])
+    plt.grid()
+    plt.xticks(np.arange(min(in_degree_sequence), max(in_degree_sequence) + 1, 1.0))
 
-    ax4 = fig.add_subplot(axgrid[2:, 2:])
-    ax4.bar(*np.unique(out_degree_sequence, return_counts=True))
-    ax4.set_title("Out-degree Histogram")
-    ax4.set_xlabel("Degree")
-    ax4.set_ylabel("# of Nodes")
+    ax3 = fig.add_subplot(axgrid[1, 0])
+    ax3.bar(k_out, p_k_out)
+    ax3.set_xlabel("k_out")
+    ax3.set_ylabel("degree distribution")
+    plt.grid()
+    plt.xlim([-0.5, max(out_degree_sequence)])
+    plt.ylim([0, 1])
+    plt.xticks(np.arange(min(out_degree_sequence), max(out_degree_sequence) + 1, 1.0))
+    plt.yticks(np.arange(0, 1 + 0.05, 0.1))
+
+    ax4 = fig.add_subplot(axgrid[1, 1])
+    ax4.scatter(k_out, p_k_out, s=100)
+    ax4.set_xlabel("k_out")
+    ax4.set_ylabel("degree distribution")
+    ax4.set_yscale('log')
+
+    plt.xlim([-0.5, max(out_degree_sequence) + 0.5])
+    plt.ylim([0.0001, 1])
+    plt.xticks(np.arange(min(out_degree_sequence), max(out_degree_sequence) + 1, 1.0))
 
     fig.tight_layout()
+    plt.grid()
+    plt.savefig('degree_analysis.png')
+    plt.show()
+
+    _plot_map_degree(graph)
+
+
+def _plot_map_degree(graph):
+
+    graph_coloring = coloring.greedy_color(graph, strategy='largest_first')
+
+    for key, value in graph_coloring.items():
+        graph.nodes[key]['color'] = value
+
+    H = nx.Graph()
+    H.add_nodes_from(sorted(graph.nodes(data=True), key=lambda x: x[1]['color']))
+    H.add_edges_from(graph.edges(data=True))
+
+    pos = {n[0]: (n[1]['lat'], n[1]['lon']) for n in H.nodes(data=True)}
+    colors_raw = [n[1]['color']+1 for n in H.nodes(data=True)]
+
+    colors = list(map(lambda x: x/max(colors_raw), colors_raw))
+
+    norm = matplotlib.colors.LogNorm(vmin=min(colors), vmax=max(colors))
+
+    plt.figure(figsize=(14, 10), dpi=300)
+    nx.draw(H, pos, node_size=list(map(lambda x: x**3, colors_raw)), node_color=norm(colors), arrowsize=1, edge_color='#ddd',
+            cmap=plt.cm.GnBu)
+    # plt.gca().invert_yaxis()
+    plt.gca().invert_xaxis()
+    plt.tight_layout()
+    plt.axis("off")
+    plt.savefig('map_degree.png')
+    plt.show()
+
+
+def connected_components_analysis(graph):
+
+    print(f"Is Strongly Connected: {nx.is_strongly_connected(graph)}")
+    print(f"Is Weakly Connected: {nx.is_weakly_connected(graph)}")
+
+    strongly_connected_components = list(sorted(nx.strongly_connected_components(graph), key=len, reverse=True))
+
+    #strongly_connected_components = [c for c in strongly_connected_components if len(c) > 1]
+
+    S = [G.subgraph(c).copy() for c in strongly_connected_components]
+
+    print(f"Components Size: {[len(c) for c in S]}")
+    print(f"Diameter of Largest Component: {nx.diameter(S[0])}")
+
+    colors = list(range(len(strongly_connected_components)))
+    color_id = 0
+    for c in strongly_connected_components:
+        color = colors[color_id]
+        for n in c:
+            graph.nodes[n]['color'] = color
+        color_id += 1
+
+    pos = {n[0]: (n[1]['lat'], n[1]['lon']) for n in graph.nodes(data=True)}
+
+    node_color = []
+    for n in graph.nodes(data=True):
+        node_color.append(n[1]['color'])
+
+    plt.figure(figsize=(14, 10), dpi=300)
+    nx.draw(graph, pos, node_size=5, node_color=node_color, arrowsize=3, vmin=1, vmax=6, edge_color='#ccc',
+            cmap=plt.cm.Set1)
+    # plt.gca().invert_yaxis()
+    plt.gca().invert_xaxis()
+    plt.tight_layout()
+    plt.axis("off")
+    plt.show()
+
+    _plot_map_connected(graph)
+
+
+def _plot_map_connected(graph):
+    graph = graph.to_undirected()
+    graph_coloring = coloring.greedy_color(graph, strategy='connected_sequential_bfs')
+
+    for key, value in graph_coloring.items():
+        graph.nodes[key]['color'] = value
+
+    H = nx.Graph()
+    H.add_nodes_from(sorted(graph.nodes(data=True), key=lambda x: x[1]['color']))
+    H.add_edges_from(graph.edges(data=True))
+
+    pos = {n[0]: (n[1]['lat'], n[1]['lon']) for n in H.nodes(data=True)}
+    colors_raw = [n[1]['color']+1 for n in H.nodes(data=True)]
+
+    colors = list(map(lambda x: x/max(colors_raw), colors_raw))
+
+    plt.figure(figsize=(14, 10), dpi=300)
+    nx.draw(H, pos, node_size=list(map(lambda x: x**3, colors_raw)), edge_color='#ddd', node_color=colors,
+            arrowsize=1, cmap=plt.cm.GnBu)
+    # plt.gca().invert_yaxis()
+    plt.gca().invert_xaxis()
+    plt.tight_layout()
+    plt.axis("off")
     plt.show()
 
 
@@ -59,43 +187,6 @@ def bridge_analysis(graph):
 
     plt.figure(figsize=(14, 10), dpi=300)
     nx.draw(graph, pos, node_size=3, node_color="b", arrowsize=3, edge_color=colors)
-    # plt.gca().invert_yaxis()
-    plt.gca().invert_xaxis()
-    plt.tight_layout()
-    plt.axis("off")
-    plt.show()
-
-
-def connected_components_analysis(graph):
-    graph_undirected = graph.to_undirected()
-
-    connected_components = list(sorted(nx.connected_components(graph_undirected), key=len, reverse=True))
-
-    S = [G.subgraph(c).copy() for c in connected_components]
-
-    print(f"Is Connected: {nx.is_connected(graph_undirected)}")
-    print(f"Is Strongly Connected: {nx.is_strongly_connected(graph)}")
-    print(f"Is Weakly Connected: {nx.is_weakly_connected(graph)}")
-    print(f"Components Size: {[len(c) for c in connected_components]}")
-    print(f"Diameter of Largest Component: {nx.diameter(S[0].to_undirected())}")
-
-    colors = [1, 2, 3, 4, 5, 6]
-    color_id = 0
-    for c in connected_components:
-        color = colors[color_id]
-        for n in c:
-            graph.nodes[n]['color'] = color
-        color_id += 1
-
-    pos = {n[0]: (n[1]['lat'], n[1]['lon']) for n in graph.nodes(data=True)}
-
-    node_color = []
-    for n in graph.nodes(data=True):
-        node_color.append(n[1]['color'])
-
-    plt.figure(figsize=(14, 10), dpi=300)
-    nx.draw(graph, pos, node_size=5, node_color=node_color, arrowsize=3, vmin=1, vmax=6, edge_color='#ccc',
-            cmap=plt.cm.Set1)
     # plt.gca().invert_yaxis()
     plt.gca().invert_xaxis()
     plt.tight_layout()
@@ -184,82 +275,28 @@ def plot(graph):
     plt.show()
 
 
-def plot_map_degree(graph):
-    graph = graph.to_undirected()
-    graph_coloring = coloring.greedy_color(graph, strategy='largest_first')
-
-    for key, value in graph_coloring.items():
-        graph.nodes[key]['color'] = value
-
-    H = nx.Graph()
-    H.add_nodes_from(sorted(graph.nodes(data=True), key=lambda x: x[1]['color']))
-    H.add_edges_from(graph.edges(data=True))
-
-    pos = {n[0]: (n[1]['lat'], n[1]['lon']) for n in H.nodes(data=True)}
-    colors_raw = [n[1]['color']+1 for n in H.nodes(data=True)]
-
-    colors = list(map(lambda x: x/max(colors_raw), colors_raw))
-
-    plt.figure(figsize=(14, 10), dpi=300)
-    nx.draw(H, pos, node_size=list(map(lambda x: x**3, colors_raw)), node_color=colors, arrowsize=1, edge_color='#ddd',
-            cmap=plt.cm.GnBu)
-    # plt.gca().invert_yaxis()
-    plt.gca().invert_xaxis()
-    plt.tight_layout()
-    plt.axis("off")
-    plt.show()
-
-
-def plot_map_connected(graph):
-    graph = graph.to_undirected()
-    graph_coloring = coloring.greedy_color(graph, strategy='connected_sequential_bfs')
-
-    for key, value in graph_coloring.items():
-        graph.nodes[key]['color'] = value
-
-    H = nx.Graph()
-    H.add_nodes_from(sorted(graph.nodes(data=True), key=lambda x: x[1]['color']))
-    H.add_edges_from(graph.edges(data=True))
-
-    pos = {n[0]: (n[1]['lat'], n[1]['lon']) for n in H.nodes(data=True)}
-    colors_raw = [n[1]['color']+1 for n in H.nodes(data=True)]
-
-    colors = list(map(lambda x: x/max(colors_raw), colors_raw))
-
-    plt.figure(figsize=(14, 10), dpi=300)
-    nx.draw(H, pos, node_size=list(map(lambda x: x**3, colors_raw)), edge_color='#ddd', node_color=colors,
-            arrowsize=1, cmap=plt.cm.GnBu)
-    # plt.gca().invert_yaxis()
-    plt.gca().invert_xaxis()
-    plt.tight_layout()
-    plt.axis("off")
-    plt.show()
-
-
 G = nx.read_gexf('../graph.gexf')
 
 # Remove zero degree nodes
 zero_degree_nodes = [n for n, d in G.degree if d == 0]
 G.remove_nodes_from(zero_degree_nodes)
 
-print(f"Nodes: {len(G.nodes)}")
-print(f"Links: {len(G.edges)}")
-print(f"Average Clustering: {nx.average_clustering(G)}")
+
+#plot(G)
+#degree_analysis(G)
+connected_components_analysis(G)
+#bridge_analysis(G)
+#spanning_tree(G)
+#voronoi_cells(G)
+#betweeness_centrality(G)
+
+print(f"Average Clustering Coefficient: {nx.average_clustering(G)}")
 print(f"Degree Assortativity Coefficient: {nx.degree_assortativity_coefficient(G)}")
 
 # Summarization
 # G_summarized, _ = nx.dedensify(G, threshold=2)
 # print(f"{G_summarized}")
 
-#degree_analysis(G)
-#bridge_analysis(G)
-#connected_components_analysis(G)
-#spanning_tree(G)
-#voronoi_cells(G)
-#betweeness_centrality(G)
-#plot(G)
-#plot_map_degree(G)
-#plot_map_connected(G)
 
 
 
